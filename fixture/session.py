3fa524fe30c0dc174144fb364314dc51ec87a63e
Rusakova <wolfik2k@gmail.com>
2022-05-15 01:40:48 +0400

    6 задание, при запуске всех тестов не выполняется логаут

--- fixture/session.py
+++ fixture/session.py
@@ -15,14 +15,6 @@ class SessionHelper:
 
     def logout(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("Logout").click()
+        wd.find_element(by=By.LINK_TEXT, value="Logout").click()
+        wd.find_element(by=By.NAME, value="user")
 
-    def login_new_contact(self, username, password):
-        wd = self.app.wd
-        self.app.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").submit()
