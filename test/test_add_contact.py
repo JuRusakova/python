923d05dedaa284ba796a93b73b0dc78cd5782d4d
Rusakova <wolfik2k@gmail.com>
2022-05-15 23:18:34 +0400

    Реализована валидация фикстуры перед каждым тестом. Доработан вспомогательный класс

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,10 +3,8 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.group.create_contact(Contact(firstname="Fill", middlename="Midd", lastname="Last", title="Title", company="Com", address="Address", home="1", mobile="2", workphon="3", fax="4", email="e", email2="e2", email3="e3",
                                            homepage= "home", bday= "7", bmonth= "July", byear= "1990", aday="12", ayear="2010", amonth="February", address2= "ADDRESS", phone2= "Home", notes="Notes",
                                            nickname="Nivada"))
-    app.session.logout()
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,11 +3,7 @@
 from model.group import Group
 
 def test_add_group2(app):
-   app.session.login(username="admin", password="secret")
    app.group.create(Group(name="nhb", header="juyy", footer="fhu"))
-   app.session.logout()
 
 def test_add_group3(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
