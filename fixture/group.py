177c4a59ac295e64cddcddfd00cb5a6bb08647af
Rusakova <wolfik2k@gmail.com>
2022-05-16 17:06:44 +0400

    заработал тест модификации контакта

--- fixture/group.py
+++ fixture/group.py
@@ -42,7 +42,6 @@ class GroupHelper:
 
     def select_first_group(self):
         wd = self.app.wd
-        # select first group
         wd.find_element_by_name("selected[]").click()
 
     def modify_first_group(self, new_group_data):
@@ -57,7 +56,6 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_group_page()
 
-
     def return_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
@@ -66,72 +64,53 @@ class GroupHelper:
         wd = self.app.wd
         # init new contact creation
         wd.find_element_by_link_text("add new").click()
-        # fill new contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(cont.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(cont.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(cont.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(cont.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(cont.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(cont.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(cont.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(cont.home)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(cont.mobile)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(cont.workphon)
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(cont.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(cont.email)
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(cont.email2)
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(cont.email3)
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(cont.homepage)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(cont.bday)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(cont.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(cont.byear)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(cont.aday)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(cont.amonth)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(cont.ayear)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(cont.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(cont.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(cont.notes)
+        self.fill_contact_form(cont)
         # submit new contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
+
+    def fill_contact_form(self, cont):
+        wd = self.app.wd
+        self.change_field_value("firstname", cont.firstname)
+        self.change_field_value("middlename", cont.middlename)
+        self.change_field_value("lastname", cont.lastname)
+        self.change_field_value("nickname", cont.nickname)
+        self.change_field_value("title", cont.title)
+        self.change_field_value("company", cont.company)
+        self.change_field_value("address", cont.address)
+        self.change_field_value("home", cont.home)
+        self.change_field_value("mobile", cont.mobile)
+        self.change_field_value("workphon", cont.workphon)
+        self.change_field_value("fax", cont.fax)
+        self.change_field_value("email", cont.email)
+        self.change_field_value("email2", cont.email2)
+        self.change_field_value("email3", cont.email3)
+        self.change_field_value("homepage", cont.homepage)
+        self.change_field_value("bday", cont.bday)
+        self.change_field_value("bmonth", cont.bmonth)
+        self.change_field_value("byear", cont.byear)
+        self.change_field_value("aday", cont.aday)
+        self.change_field_value("amonth", cont.amonth)
+        self.change_field_value("ayear", cont.ayear)
+        self.change_field_value("address2", cont.address2)
+        self.change_field_value("phone2", cont.phone2)
+        self.change_field_value("notes", cont.notes)
+
+
+    def modify_first_contact(self, new_contact_data):
+        wd = self.app.wd
+        self.select_first_contact()
+        # open modification form
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        # fill group form
+        self.fill_contact_form(new_contact_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+
+    def select_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def return_to_home_page(self):
         wd = self.app.wd
@@ -145,97 +124,6 @@ class GroupHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         #allert
         wd.switch_to.alert.accept()
-
-    def edit_first_group(self, group):
-        wd = self.app.wd
-        self.open_group_page()
-        #select first group
-        wd.find_element_by_name("selected[]").click()
-        #submit edit
-        wd.find_element_by_name("edit").click()
-        #edit group name
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("update").click()
-        self.return_group_page()
-
-    def edit_first_contact(self, cont):
-        wd = self.app.wd
-        #select first contact
-        wd.find_element_by_name("selected[]").click()
-        # submit edit
-        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
-        # edit contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(cont.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(cont.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(cont.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(cont.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(cont.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(cont.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(cont.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(cont.home)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(cont.mobile)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(cont.workphon)
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(cont.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(cont.email)
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(cont.email2)
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(cont.email3)
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(cont.homepage)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(cont.bday)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(cont.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(cont.byear)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(cont.aday)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(cont.amonth)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(cont.ayear)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(cont.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(cont.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(cont.notes)
-        # submit
-        wd.find_element_by_name("update").click()
-        self.return_to_home_page()
 
     def delete_all_contacts(self):
         wd = self.app.wd
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    app.group.modify_first_contact(Contact(firstname="New firstname")
+    app.group.modify_first_contact(Contact(firstname="New firstname"))
 
-def test_modify_contact_middlname(app):
-    app.group.modify_first_contact(Contact(middlename="New middlname")
+#def test_modify_contact_middlname(app):
+    #app.group.modify_first_contact(Contact(middlename="New middlname")
