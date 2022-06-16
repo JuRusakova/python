a389299ad2207a12aeb0c4028b189824ec8e6ad5
Rusakova <wolfik2k@gmail.com>
2022-06-16 10:05:55 +0400

    20 задание 2.0

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -13,6 +13,7 @@ def test_modify_contact_firstname(app, db, check_ui):
     new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     old_contacts[index] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
