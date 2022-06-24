06af4068052cbef1cd850695da713aa42d030f49
Rusakova <wolfik2k@gmail.com>
2022-06-24 10:31:48 +0400

    20

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -9,10 +9,10 @@ def test_modify_contact(app, db, check_ui):
     old_contacts = db.get_contact_list()
     contact_random = random.choice(old_contacts)
     app.contact.modify_contact_by_id(contact_random.id, contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts_db = db.get_contact_list()
     new_contacts_ui = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts_db)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts_db, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts_db, key=Contact.id_or_max) == sorted(new_contacts_ui, key=Contact.id_or_max)
 
